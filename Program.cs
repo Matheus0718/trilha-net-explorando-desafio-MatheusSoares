e71dc0151c00cf771847ba8233bbb4cdc90e83d4
Matheus Soares <math.ss0718@gmail.com>
2025-06-25 22:41:22 -0300

    Projeto Matheus Soares de Souza

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using DesafioProjetoHospedagem.Models;
+using System.Linq;
 
 Console.OutputEncoding = Encoding.UTF8;
 
@@ -8,15 +9,17 @@ List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+// Cria a suite
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 3, valorDiaria: 40);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 20);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
